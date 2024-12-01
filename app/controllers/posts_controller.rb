c7408228f580617a1d3591f1f6b92e899ff7bc86
Erenimo3442 <ysferen.ari@gmail.com>
2024-12-01 21:59:17 +0300

    Added posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :media)
+  end
+end
