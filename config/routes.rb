6f9b36ca8c952cd9728418449d31ad3528769ae9
Erenimo3442 <ysferen.ari@gmail.com>
2024-12-01 14:34:59 +0300

    Initialized Devise gem for user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "users/show"
+  get "users/edit"
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
