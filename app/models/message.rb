fb67e4448c8a043026445eb11a917d7a5b53d5fb
Ammar Yasir Bayır <bayirammar@gmail.com>
2024-12-02 19:54:40 +0300

    added Direct Message feature

---
+++ app/models/message.rb
@@ -0,0 +1,7 @@
+# app/models/message.rb
+class Message < ApplicationRecord
+  belongs_to :sender, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
+
+  validates :body, presence: true
+end
