c9af7bf45d30ee13d749cb499d09a94d83929e7b
Yusuf Eren Arı <ysferen.ari@gmail.com>
2024-12-02 22:27:10 +0300

    Added friendship feature to direct messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,33 @@
-# app/controllers/messages_controller.rb
 class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @messages = Message.where(sender: current_user).or(Message.where(receiver: current_user))
-    @users = User.where.not(id: current_user.id)
-  end
-
-  def show
-    @other_user = User.find(params[:id])
-    @messages = Message.where(sender: [current_user, @other_user], receiver: [current_user, @other_user])
-    @new_message = Message.new(receiver: @other_user)
+    friend_ids = current_user.friends.pluck(:id)
+    @users = User.where(id: friend_ids)
+    @messages = Message.where(sender: current_user).or(Message.where(receiver: current_user)).order(created_at: :desc)
   end
 
   def new
+    friend_ids = current_user.friends.pluck(:id)
+    @users = User.where(id: friend_ids)
     @message = Message.new
   end
 
   def create
     @message = current_user.sent_messages.build(message_params)
     if @message.save
-      redirect_to message_path(@message.receiver), notice: 'Message sent!'
+      redirect_to messages_path, notice: 'Message was successfully sent.'
     else
-      redirect_back fallback_location: messages_path, alert: 'Message could not be sent.'
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @other_user = User.find(params[:id])
+    @messages = Message.where(sender: current_user, receiver: @other_user)
+                       .or(Message.where(sender: @other_user, receiver: current_user))
+                       .order(created_at: :asc)
+    @new_message = current_user.sent_messages.build(receiver: @other_user)
   end
 
   private
