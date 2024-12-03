176a423cecb0e5f5aba6840f8e6ac9ecc6005078
Ece Eylül Sen <eceylulsenn@gmail.com>
2024-12-03 09:00:31 +0300

    improved general features added avatar and title on posts improved profile page general :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :status])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :status])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :status, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :status, :avatar])
   end
 
   def after_sign_in_path_for(resource)
