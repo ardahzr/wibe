e73e33491187997ce1347581f1e89455bd20396e
Ece Eylül Sen <eceylulsenn@gmail.com>
2024-12-03 09:00:31 +0300

    improved general features added avatar and title on posts improved profile page general :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_02_151436) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_02_220906) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_02_151436) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
