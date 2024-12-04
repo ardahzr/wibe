c55c706c8b23f861f8262912e84f90f959af7165
Ammar Yasir Bayır <bayirammar@gmail.com>
2024-12-05 01:54:19 +0300

    Implement AJAX for DM feature and update conversation view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :messages
   resources :friendships, only: [:create, :update, :destroy]
   devise_for :users
   resources :posts
-  resources :messages, only: [:index, :new, :create, :show]
   resources :users, only: [:index, :show, :edit, :update]
   resources :users, only: [:show, :edit, :update]
   get 'profile', to: 'accounts#show', as: :profile
