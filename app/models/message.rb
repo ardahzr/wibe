716a2bbe1fc0bd6f7e4c6b9ce6079d9dda0457a5
Ammar Yasir Bayır <bayirammar@gmail.com>
2024-12-28 12:46:36 +0300

    Popped last changes from stash succesfully

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,11 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+  has_one_attached :attachment
 
-  validates :body, presence: true
+  validates :body, presence: true, unless: -> { attachment.attached? }
+  
+  def image?
+    attachment.content_type.start_with?('image/') if attachment.attached?
+  end
 end
