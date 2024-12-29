dfda9099cb1f7aeae29f7aa0260d2d9dca7d4c8f
Yusuf Eren Arı <ysferen.ari@gmail.com>
2024-12-30 00:42:44 +0300

    Added tests for like & message features

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @post = posts(:one)
+    @like = Like.new(user: @user, post: @post)
+  end
+
+  test "should be valid" do
+    assert @like.valid?
+  end
+
+  test "should require a user" do
+    @like.user = nil
+    assert_not @like.valid?
+  end
+
+  test "should require a post" do
+    @like.post = nil
+    assert_not @like.valid?
+  end
 end
